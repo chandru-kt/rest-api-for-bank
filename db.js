468d2dd70d20e961c64486dc0d1dc154cc7efc53
CHANDRU_K_T <94432813+chandru-kt@users.noreply.github.com>
2023-02-27 21:14:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,9 @@ const pool = new Pool({
 	database: 'bank_det',
 	password: 'l3AK61KAp0yrosaj2CFvDaiayUYkWTTD',
 	port: 5432,
+	ssl: 'true',
   })
+  
 
 const getBranch = (request, response) => {
 
@@ -25,11 +27,11 @@ const SearchbyValue = (request, response) => {
 	const q = request.query.q;
 	const limit = request.query.limit;
 	const offset = request.query.offset;
+
 	pool.query('SELECT * FROM bank_branches WHERE ifsc ILIKE $1 or bank_id ILIKE $1 or branch ILIKE $1 or address ILIKE $1 or city ILIKE $1 or district ILIKE $1 or state ILIKE $1 or bank_name ILIKE $1  ORDER BY ifsc DESC LIMIT $2 OFFSET $3 ', ['%'+q+'%', limit,offset ], (error, results) => {
 	if (error) {
-	throw error
+		throw error
 	}
-	results = '"branches": ['+json(results.rows)+'] }';
 	response.status(200).json.parse(results)
 })
 }
